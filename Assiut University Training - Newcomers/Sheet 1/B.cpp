8239847bcce98890df8323cee567fd7e7a176a5b
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-01-07 13:03:26 +0200

    Update B.cpp

--- Assiut University Training - Newcomers/Sheet 1/B.cpp
+++ Assiut University Training - Newcomers/Sheet 1/B.cpp
@@ -1,1 +1,13 @@
-
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+int main() {
+    int a;
+    ll b;
+    char ch;
+    float f;
+    double d;
+    cin>>a>>b>>ch>>f>>d;
+    cout<<a<<endl<<b<<endl<<ch<<endl<<f<<endl<<d<<endl;
+    }
