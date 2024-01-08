cff40a7ca02d4a455dfca5eff576b3e274509a70
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-01-08 17:11:06 +0200

    Create C.cpp

---
+++ Assiut University Training - Newcomers/Sheet 1/C.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+int main() {
+    ll a,b;
+    cin>>a>>b;
+    cout<<a <<" + " <<b <<" = " <<a+b<<endl;
+    cout<<a <<" * " <<b <<" = " <<a*b<<endl;
+    cout<<a <<" - " <<b <<" = " <<a-b<<endl;
+    return 0;
+}
