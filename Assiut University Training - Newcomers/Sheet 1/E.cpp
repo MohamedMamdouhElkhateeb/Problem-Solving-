cd86dd7daafe878d324e220d55f03c923a7304f4
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-01-08 17:11:50 +0200

    Create E.cpp

---
+++ Assiut University Training - Newcomers/Sheet 1/E.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+int main() {
+    double n;
+    cin>>n;
+    cout<< fixed << setprecision(9)<<n*n*3.141592653;
+    return 0;
+}
