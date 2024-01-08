6d3b6a7dccc7dd7c78c4b20272f0f8e24857eb6e
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-01-08 17:11:30 +0200

    Create D.cpp

---
+++ Assiut University Training - Newcomers/Sheet 1/D.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+int main() {
+    ll a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<"Difference = "<<(a*b)-(c*d)<<endl ;
+    return 0;
+}
