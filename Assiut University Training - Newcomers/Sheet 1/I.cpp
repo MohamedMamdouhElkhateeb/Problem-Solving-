a789210db0d1ff9e94ac93f01e0150f42546e355
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-01-10 11:47:52 +0200

    Create I.cpp

---
+++ Assiut University Training - Newcomers/Sheet 1/I.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+int main() {
+    ll n,x;
+    cin>>n>>x;
+    if(n>x||n==x){
+        cout<<"Yes";
+    }
+    else cout<<"No";
+}
