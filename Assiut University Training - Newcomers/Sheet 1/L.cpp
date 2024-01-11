098b0c16e44bbc986a38775cea39fb7a18a768a8
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-01-11 13:36:12 +0200

    Create L.cpp

---
+++ Assiut University Training - Newcomers/Sheet 1/L.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+int main() {
+    string s1,s2,s3,s4;
+    cin>>s1>>s2;
+    cin>>s3>>s4;
+    if(s2==s4){
+        cout<<"ARE Brothers";
+
+    }else cout<<"NOT";
+    }
