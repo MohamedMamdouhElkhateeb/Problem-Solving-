a898a7e81d5cc8a5d0a30a524c8b4d858671d728
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-01-13 20:00:30 +0200

    Create O.cpp

---
+++ Assiut University Training - Newcomers/Sheet 1/O.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+int main() {
+    ll a,c;
+    char b;
+    cin>>a>>b>>c;
+    if(b=='+'){
+        cout<<a+c;
+    }
+    else if(b=='-'){
+        cout<<a-c;
+    }
+    else if(b=='*'){
+        cout<<a*c;
+    }
+    else if(b=='/'){
+        cout<<a/c;
+    }
+    }
+ 
