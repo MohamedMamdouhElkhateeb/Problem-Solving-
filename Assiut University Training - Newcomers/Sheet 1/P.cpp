6c8480c923dad7a3dc445dca059c06ff72de0d04
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-01-14 06:39:53 +0200

    Create P.cpp

---
+++ Assiut University Training - Newcomers/Sheet 1/P.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+int main() {
+    ll a,c;
+    cin>>a;
+    a=a/1000;
+    if(a%2==0){
+        cout<<"EVEN";
+    }
+    else cout<<"ODD";
+    }
+ 
