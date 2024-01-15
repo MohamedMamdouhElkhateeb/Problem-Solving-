9b85719b306d5c6c40cc34bf8bb4fd2384ea80b8
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-01-15 06:16:30 +0200

    Create Q.cpp

---
+++ Assiut University Training - Newcomers/Sheet 1/Q.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+
+int main() {
+   //cout << fixed << setprecision(0);
+   //pow (a,b)= b*log(a)
+    float x,y;
+    cin>>x>>y;
+    if(x>0&&y>0){
+        cout<<"Q1";
+    }
+    else if(x<0&&y>0){
+        cout<<"Q2";
+    }
+    else if(x<0&&y<0){
+        cout<<"Q3";
+    }
+    else if(x>0&&y<0){
+        cout<<"Q4";
+    }
+    else if(x>0&&y==0){
+        cout<<"Eixo X";
+    }
+    else if(x<0&&y==0){
+        cout<<"Eixo X";
+    }
+    else if(x==0&&y<0){
+        cout<<"Eixo Y";
+    }
+    else if(x==0&&y>0){
+        cout<<"Eixo Y";
+    }
+    else cout<<"Origem";
+        }
+
+
+
+
+
+ 
