393aa6f290bfb2230a01744e569c7ce284a41b45
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-01-19 13:34:49 +0200

    Create U.cpp

---
+++ Assiut University Training - Newcomers/Sheet 1/U.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+
+int main() {
+   //cout << fixed << setprecision(0);
+   //pow (a,b)= b*log(a)
+    double x;
+    cin>>x;
+    int n = x;
+    double res = x-n;
+    if(res==0){
+        cout<<"int "<<n;
+    }
+    else cout<<"float "<<n <<" "<<res;
+        }
+
+
+
+
+
+ 
