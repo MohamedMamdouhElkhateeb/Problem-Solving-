c988d323c42c90de2456ccb2d147ed331ac85452
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-01-25 22:00:22 +0200

    Update A.cpp

---
+++ Assiut University Training - Newcomers/Sheet 2/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+int main() {
+    ll a;
+    cin>>a;
+    for(int i=1;i<=a;i++){
+    cout<<i<<endl;
+}
+
+}
+
+
+
+
+
+
+
