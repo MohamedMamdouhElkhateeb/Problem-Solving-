238225c8d81c015d2a7699326519255b71089c41
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-01-25 22:00:54 +0200

    Create B.cpp

---
+++ Assiut University Training - Newcomers/Sheet 2/B.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+int main() {
+    ll n;
+    cin>>n;
+    if(n<2){
+        cout<<-1;
+    }
+    else {
+        for(int i=2;i<=n;i+=2){
+
+            cout<<i<<endl;
+        }
+    }
+
+
+    }
+
+
+
+
+
+
+
+
