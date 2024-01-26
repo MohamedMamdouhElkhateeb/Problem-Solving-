8dc8507cb8ecdab984756b7b3c8b960542c999e3
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-01-26 23:58:12 +0200

    Update S.cpp

--- Assiut University Training - Newcomers/Sheet 1/S.cpp
+++ Assiut University Training - Newcomers/Sheet 1/S.cpp
@@ -10,15 +10,12 @@ int main() {
    cin>>n;
    if(n>=0&&n<=25){
        cout<<"Interval [0,25]"<<endl;
-
    }
    else if(n>25&&n<=50){
         cout<<"Interval (25,50]"<<endl;
-
     }
    else if(n>50&&n<=75){
        cout<<"Interval (50,75]"<<endl;
-
    }
    else if(n>75&&n<=100){
        cout<<"Interval (75,100]"<<endl;
