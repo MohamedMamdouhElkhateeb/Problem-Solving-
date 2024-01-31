9f1120f487656b164f372553ae52becd47c3a6f1
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-01-31 21:40:46 +0200

    Update E.cpp

--- Assiut University Training - Newcomers/Sheet 2/E.cpp
+++ Assiut University Training - Newcomers/Sheet 2/E.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
 
+int main() {
+    ll t, n, maxn= 0;
+    cin >>t;
+    while(t--){
+        cin>>n;
+        if(n>maxn){
+            maxn=n;
+        }
+    }
+    cout<<maxn<<endl;
+
+    return 0;
+}
