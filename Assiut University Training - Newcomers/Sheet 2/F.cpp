6f2145850299f24a6b956b980a59e37b3857d49c
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-02-01 23:49:59 +0200

    Update F.cpp

--- Assiut University Training - Newcomers/Sheet 2/F.cpp
+++ Assiut University Training - Newcomers/Sheet 2/F.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+int main() {
+    ll n;
+    cin>>n;
+    for(int i=1;i<=12;i++){
+        cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+    }
+}
 
+
+
+
+
+
+ 
