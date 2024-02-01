eedfa731611a1b81eb5b68517c9ac1c804586602
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-02-02 00:12:57 +0200

    Update A.cpp

---
+++ Assiut University Training - Newcomers/Contest #1/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+int main() {
+    ll a,b;
+    cin>>a>>b;
+    double res = b/(1.0 -a/ 100.0);
+    cout<<fixed<<setprecision(2)<<res;
+
+}
+
+
+
+
+
+
+
+
+ 
