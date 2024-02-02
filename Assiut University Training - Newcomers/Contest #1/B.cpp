67a7cd425c6b1329c9ecb1d5b82a1dcc4e406fc8
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-02-03 00:33:07 +0200

    Update B.cpp

--- Assiut University Training - Newcomers/Contest #1/B.cpp
+++ Assiut University Training - Newcomers/Contest #1/B.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+int main() {
+    ll a,b,k;
+    cin>>a>>b>>k;
+    if(a%k==0&&b%k==0){
+        cout<<"Both"<<endl;
+    }
+    else if(a%k==0&&b%k!=0){
+        cout<<"Memo";
+    }
+    else if(b%k==0&&a%k!=0){
+        cout<<"Momo"<<endl;
+    }
+    else cout<<"No One";
 
+    }
+
+
+
+
+
+
+ 
