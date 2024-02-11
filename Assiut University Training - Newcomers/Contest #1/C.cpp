40254c37b5535c333a0d31ff891fde5359de2ae1
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-02-12 01:32:35 +0200

    Update C.cpp

--- Assiut University Training - Newcomers/Contest #1/C.cpp
+++ Assiut University Training - Newcomers/Contest #1/C.cpp
@@ -1,1 +1,23 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
+int main() {
+    char ch;
+    cin>>ch;
+    if(ch>=97&&ch<=121){
+        char h = ch+1;
+        cout<<h<<endl;
+    }
+    else cout<<'a';
 
+
+
+}
+
+
+
+
+
+
+ 
