542f04ce1090cd04b21729083e673177e58be544
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-02-14 07:51:57 +0200

    Update D.cpp

--- Assiut University Training - Newcomers/Contest #1/D.cpp
+++ Assiut University Training - Newcomers/Contest #1/D.cpp
@@ -1,1 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long num1,num2,num3,result,res1,res2,res3,res4,res5,res6;
+    cin>>num1>>num2>>num3>>result;
+    res1 = (num1 * num2) + num3;
+    res2 = (num1 * num2) - num3;
+    res3 = num1 + (num2 * num3);
+    res4 = num1 - (num2 * num3);
+    res5 = (num1 - num2) + num3;
+    res6 = (num1 + num2) - num3;
 
+    if(res1==result||res2==result||res3==result||res4==result||res5==result||res6==result){
+        cout<<"YES"<<endl;
+    }
+    else {
+        cout<<"NO"<<endl;
+    }
+}
