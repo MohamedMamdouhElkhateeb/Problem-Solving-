504616eb4952402418933f400b161416d901cadb
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-02-15 14:12:03 +0200

    Update E.cpp

--- Assiut University Training - Newcomers/Contest #1/E.cpp
+++ Assiut University Training - Newcomers/Contest #1/E.cpp
@@ -1,1 +1,43 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+#define dd double
+using namespace std;
 
+int main() {
+
+    // cout << fixed << setprecision(4);
+    //pow (a,b)= b*log(a)
+    ll a,b;
+
+
+    cin>>a>>b;
+    if(a==0&&b==0){
+        cout<<"NO";
+    }
+    else {
+
+
+        ll res = ::abs(a - b);
+        if (res > 1) {
+            cout << "NO";
+        } else cout << "YES";
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
