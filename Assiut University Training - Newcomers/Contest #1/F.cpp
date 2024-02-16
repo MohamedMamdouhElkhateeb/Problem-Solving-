55138ca9bbf5cfb0b69b2e3f96c98f6cb53d4ceb
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-02-16 23:32:46 +0200

    Update F.cpp

--- Assiut University Training - Newcomers/Contest #1/F.cpp
+++ Assiut University Training - Newcomers/Contest #1/F.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+#define dd double
+using namespace std;
 
+int main() {
+   // cout << fixed << setprecision(4);
+    //pow (a,b)= b*log(a)
+  ll a,b;
+  cin>>a>>b;
+  ll res=a^b;
+  cout<<res;
+}
+
+
+
+
+
+
+ 
