f975c7b3775bca5c5a3709309b55bce7e9646db5
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-02-22 11:11:28 +0200

    Update H.cpp

--- Assiut University Training - Newcomers/Contest #1/H.cpp
+++ Assiut University Training - Newcomers/Contest #1/H.cpp
@@ -1,1 +1,34 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+#define dd double
+using namespace std;
+
+int main() {
+
+    // cout << fixed << setprecision(4);
+    //pow (a,b)= b*log(a)
+    dd a, b, c;
+    cin >> a >> b >> c;
+    dd res = (a * b) / c;
+    ll num = res;
+    dd fres = res - num;
+    if(fres>0){
+        cout << "double";
+    }
+   else if ( num <= 2147483647) {
+        cout << "int";
+    }
+
+    else cout << "long long";
+
+
+}
+
+
+
+
+
+
+
 
