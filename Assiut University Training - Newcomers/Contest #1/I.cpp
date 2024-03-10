90176cc4684917fd71df20f619b4d6ad98993bdd
Mohamed Mamdouh Elkhateeb <120528467+MohamedMamdouhElkhateeb@users.noreply.github.com>
2024-03-10 15:16:53 +0200

    Update I.cpp

--- Assiut University Training - Newcomers/Contest #1/I.cpp
+++ Assiut University Training - Newcomers/Contest #1/I.cpp
@@ -1,1 +1,20 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ll long long
+using namespace std;
 
+int main() {
+    ll n;
+    cin>>n;
+    if(n==10||n==20||n==30||n==40||n==50||n==60||n==70||n==80||n==90){
+        cout<<"YES";
+    }
+    else{
+        ll r1=n/10,r2=n-10*r1;
+        if(r1%r2==0||r2%r1==0){
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO";
+    }
+
+}
